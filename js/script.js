2ccfd5658edc09fd9a97197c1503bb880a0a1a9f
churanta <churantamondal321@gmail.com>
2023-08-02 19:13:34 +0530

    first commit

--- js/script.js
+++ js/script.js
@@ -13,7 +13,9 @@ const sr = ScrollReveal ({
 	reset: true
 });
 
-sr.reveal('.hero-text',{delay:200, origin:'top'});
+sr.reveal('.hero-text', { delay: 200, origin: 'top' });
+sr.reveal('.hero-text2', { delay: 300, origin: 'bottom' });
+sr.reveal('.center', { delay: 200, origin: 'bottom' });
 sr.reveal('.hero-img',{delay:450, origin:'top'});
 sr.reveal('.icons',{delay:500, origin:'left'});
 sr.reveal('.scroll-down',{delay:500, origin:'right'});
