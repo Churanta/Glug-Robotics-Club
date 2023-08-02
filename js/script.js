6d45caffe563ab69fb4bff5e0bc6ce76bca1a3cb
churanta <churantamondal321@gmail.com>
2023-08-02 20:40:46 +0530

    first commit

--- js/script.js
+++ js/script.js
@@ -16,6 +16,7 @@ const sr = ScrollReveal ({
 sr.reveal('.hero-text', { delay: 200, origin: 'top' });
 sr.reveal('.hero-text2', { delay: 300, origin: 'bottom' });
 sr.reveal('.center', { delay: 200, origin: 'bottom' });
+sr.reveal('.card', { delay: 200, origin: 'bottom' });
 sr.reveal('.hero-img',{delay:450, origin:'top'});
 sr.reveal('.icons',{delay:500, origin:'left'});
 sr.reveal('.scroll-down',{delay:500, origin:'right'});
@@ -36,15 +37,61 @@ window.addEventListener('scroll', () => {
 
 
 
-  // Function to scroll to the bottom of the page
-  function scrollToBottom() {
-    window.scrollTo({
-      top: document.body.scrollHeight,
-      behavior: 'smooth'
-    });
+// Function to handle smooth scrolling
+function smoothScrollToSection(event) {
+  event.preventDefault(); // Prevent default anchor click behavior
+
+  // Check if the up class is present
+  if (this.classList.contains("up")) {
+    // Scroll to the top of the page smoothly
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  } else {
+    // Get the target section element based on the href attribute of the anchor
+    const targetSection = document.querySelector(this.getAttribute("href"));
+
+    // Check if the target section exists
+    if (targetSection) {
+      // Scroll to the target section smoothly
+      targetSection.scrollIntoView({ behavior: "smooth" });
+    }
   }
+}
 
-  // Attach the click event listener to the menu icon
-  document.getElementById('menu-icon').addEventListener('click', scrollToBottom);
+function handleScroll() {
+  const scrollDownButton = document.querySelector(".scroll-down a");
+
+  // Show/hide the scroll-down button based on the scroll position
+  if (window.scrollY + window.innerHeight >= document.body.scrollHeight) {
+    scrollDownButton.parentElement.classList.add("up"); // Add "up" class to parent element
+    scrollDownButton.classList.add("hide");
+  } else {
+    scrollDownButton.parentElement.classList.remove("up"); // Remove "up" class from parent element
+    scrollDownButton.classList.remove("hide");
+  }
+}
+
+// Add event listener to the scroll-down button
+const scrollDownButton = document.querySelector(".scroll-down a");
+scrollDownButton.addEventListener("click", smoothScrollToSection);
+
+// Add event listener to handle scrolling and button visibility
+window.addEventListener("scroll", handleScroll);
 
 
+
+// Function to handle smooth scrolling to the "joinus" section
+function smoothScrollToJoinUs(event) {
+  event.preventDefault(); // Prevent default anchor click behavior
+
+  const joinUsSection = document.getElementById("joinus");
+
+  // Check if the target section exists
+  if (joinUsSection) {
+    // Scroll to the "joinus" section smoothly
+    joinUsSection.scrollIntoView({ behavior: "smooth" });
+  }
+}
+
+// Add event listener to the link/button that triggers the smooth scroll
+const scrollToJoinUsButton = document.getElementById("scrollToJoinUs");
+scrollToJoinUsButton.addEventListener("click", smoothScrollToJoinUs);
